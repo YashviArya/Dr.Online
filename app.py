7fbeb0e92df5691cb25c0e53c0a1f000fccb63f6
Yashvi Arya <150829653+YashviArya@users.noreply.github.com>
2025-05-22 11:31:52 +0000

    Login , signup added

--- app.py
+++ app.py
@@ -1,11 +1,24 @@
-
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
+@app.route("/home")
+@app.route("/dashboard")
 @app.route("/")
 def hello_world():
-    return render_template('base.html')
+    return render_template('patientdashboard.html')
+
+
+@app.route("/signup")
+def signup():
+    return render_template('signup.html')
+
+
+@app.route("/login")
+def login():
+    return render_template('login.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')
